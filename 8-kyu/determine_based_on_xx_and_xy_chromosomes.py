e130f121790c5f025876eb10f479c5ee404a4694
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2023-12-29 14:53:50 +0200

    done with few other exercises

--- 8-kyu/determine_based_on_xx_and_xy_chromosomes.py
+++ 8-kyu/determine_based_on_xx_and_xy_chromosomes.py
@@ -3,3 +3,7 @@ DAUGHTER =  "Congratulations! You\'re going to have a daughter."
 
 def chromosome_check(chromosome):
     return SON if chromosome == "XY" else DAUGHTER
+
+### Another cool solution, using formatting
+def chromosome_check(sperm):
+    return 'Congratulations! You\'re going to have a {}.'.format('son' if 'Y' in sperm else 'daughter')
